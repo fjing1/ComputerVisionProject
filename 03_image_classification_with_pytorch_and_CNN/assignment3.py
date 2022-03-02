4af03ac9f0497e3b64653784b6813659b4d1c6bf
Felix Jing <32583955+fjing1@users.noreply.github.com>
2022-03-01 20:14:38 -0700

    wip assignment3, no error so far

--- 03_image_classification_with_pytorch_and_CNN/assignment3.py
+++ 03_image_classification_with_pytorch_and_CNN/assignment3.py
@@ -40,30 +40,92 @@ print('Down size:', x.shape)
 # If the sample from question 3 is then passed through a 2x2 MaxPooling layer
 # create the pooling layer and pass in one data sample as input, then printout the resulting matrix size
 
-
+x = F.max_pool2d(x, 2, 2)
+print(x.shape)
+#result is torch.Size([10, 2, 12, 12])
 
 # Define a convolutional neural network
 # Define a CNN model that can be trained on the Fashion-MNIST dataset.
 # The model should contain two convolutional layers, two pooling layers, and two fully connected layers.
 # You can use any number of neurons per layer so long as the model takes in a 28x28 image and returns an output of 10.
 # and then printout the count of parameters of your model
+class CNN_a3(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3, 1)
+        self.conv2 = nn.Conv2d(6, 16, 3, 1)
 
+        self.fc1 = nn.Linear(5*5*16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-
+    def forward(self, X):
+        X = F.relu(self.conv1(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = F.relu(self.conv2(X))
+        X = F.max_pool2d(X, 2, 2)
+        X = X.view(-1, 5*5*16)
+        X = F.relu(self.fc1(X))
+        X = F.relu(self.fc2(X))
+        X = F.log_softmax(self.fc3(X), dim=1)
+        return X
+#Initiate our model
+model = CNN_a3()
+print(model)
 # Define loss function & optimizer
+
 # Define a loss function called "criterion" and an optimizer called "optimizer".
 # You can use any loss functions and optimizer you want,
 # although we used Cross Entropy Loss and Adam (learning rate of 0.001) respectively.
-
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 # Train and test the model
 # try with any epochs you want
 # and printout some interim results
+import time
+start_time = time.time()
+epochs = 8
+for i in range(epochs):
+    trn_corr = 0
+    tst_corr = 0
 
+    # Run the training batches
+    for b, (X_train, y_train) in enumerate(train_loader):
+        b += 1
 
+        y_pred = model(X_train)
+        loss = criterion(y_pred, y_train)
+
+        predicted = torch.max(y_pred, 1)[1]
+        batch_corr = (predicted == y_train).sum()
+        trn_corr += batch_corr
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if b % 3000 == 0:
+            print(f'epoch:{i} batch:{b} loss: {loss.item()} accuracy: {trn_corr.item() * 100 / (10 * b)}%')
+
+    # Run the testing batches
+    with torch.no_grad():
+        for b, (X_test, y_test) in enumerate    (test_loader):
+            y_val = model(X_test)
+
+            predicted = torch.max(y_val.data, 1)[1]
+            tst_corr += (predicted == y_test).sum()
+    print(f'Test accuracy: {tst_corr.item() * 100 / (len(test_data))}%')
 # Remember, always experiment with different architecture and different hyper-parameters, such as
 # different activation function, different loss function, different optimizer with different learning rate
 # different size of convolutional kernels, and different combination of convolutional layers/pooling layers/FC layers
 # to make the best combination for solving your problem in real world
-
+x = 2019
+plt.figure(figsize=(1,1))
+plt.imshow(test_data[x][0].reshape((28,28)))
+plt.show()
+model.eval()
+with torch.no_grad():
+    new_pred = model(test_data[x][0].reshape(1,1,28,28)).argmax()
+    print(new_pred.item())
